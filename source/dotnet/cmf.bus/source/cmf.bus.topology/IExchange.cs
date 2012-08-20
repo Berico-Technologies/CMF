0a2d11a3d1cac3c980e095e5b2464ca903963c3b
John Ruiz <jruiz@dev.johnruiz.com>
2012-08-19 21:21:20 -0400

    Sunday night stopping point.  checking in for others' reference

---
+++ source/dotnet/cmf.bus/source/cmf.bus.topology/IExchange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cmf.bus.topology
+{
+    public interface IExchange
+    {
+        string Hostname { get; set; }
+        string VirtualHost { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ source/dotnet/cmf.bus/source/cmf.bus.topology/ITopologyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cmf.bus.topology
+{
+    public interface ITopologyService
+    {
+        IExchange GetExchangeForTopic(string topic);
+    }
+}
