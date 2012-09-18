e4baa7d8cd04fe6467d8a21b5d2662b6697b4187
Jared Holmberg <jholmberg@bericotechnologies.com>
2012-09-18 12:03:53 -0400

    Added comments to the IEventConsumer Subscribe method.

--- source/dotnet/source/cmf.bus/cmf.eventing/IEventConsumer.cs
+++ source/dotnet/source/cmf.bus/cmf.eventing/IEventConsumer.cs
@@ -11,6 +11,11 @@ namespace cmf.eventing
     {
         void Subscribe(IEventHandler handler);
 
+        /// <summary>
+        /// Registers a handler via an Action that listens for a strongly typed event (TEvent) from the Bus.
+        /// </summary>
+        /// <typeparam name="TEvent">The specified type of event to listen for (e.g. - EntityQuery).</typeparam>
+        /// <param name="handler">Action that listens for the TEvent type. The IDictionary received are the message headers sent with the event.</param>
         void Subscribe<TEvent>(Action<TEvent, IDictionary<string, string>> handler) where TEvent : class;
     }
 }
