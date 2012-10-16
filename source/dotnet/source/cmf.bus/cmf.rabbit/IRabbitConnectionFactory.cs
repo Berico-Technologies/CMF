d0dc43bcc9d441cddac2b463c64ed645309491e5
John Ruiz <jruiz@bericotechnologies.com>
2012-10-16 10:03:39 -0400

    another temporary checkin until I Get to work

---
+++ source/dotnet/source/cmf.bus/cmf.rabbit/IRabbitConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RabbitMQ.Client;
+
+using cmf.rabbit.topology;
+
+namespace cmf.rabbit
+{
+    public interface IRabbitConnectionFactory
+    {
+        IConnection ConnectTo(Exchange exchange);
+    }
+}
