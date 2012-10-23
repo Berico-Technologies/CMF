7a2f72d9a922dae5c9bf9c7a7aa0ebcb685a8d20
Asa Martin <amartin@bericotechnologies.com>
2012-10-23 07:32:01 -0400

    added basic connection factory for use without certificates and added virtual host settign to certificate connection factory

--- source/java/src/main/java/cmf/eventing/berico/RpcFilter.java
+++ source/java/src/main/java/cmf/eventing/berico/RpcFilter.java
@@ -8,88 +8,76 @@ import java.util.UUID;
 
 import org.joda.time.Duration;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cmf.bus.berico.EnvelopeHelper;
 
 public class RpcFilter implements IInboundEventProcessor, IOutboundEventProcessor {
 
-	protected List<UUID> sentRequests;
-    protected Logger log;
-    
-    
-    public RpcFilter() {
-    	this.sentRequests = new ArrayList<UUID>();
-    }
-    
-    
-	@Override
-	public void processOutbound(ProcessingContext context) {
+    protected List<UUID> sentRequests;
+    protected static final Logger log = LoggerFactory.getLogger(RpcFilter.class);
 
-		EnvelopeHelper env = new EnvelopeHelper(context.getEnvelope());
-		
-        if (env.IsRequest())
-        {
+    public RpcFilter() {
+        this.sentRequests = new ArrayList<UUID>();
+    }
+
+    @Override
+    public void processOutbound(ProcessingContext context) {
+
+        EnvelopeHelper env = new EnvelopeHelper(context.getEnvelope());
+
+        if (env.IsRequest()) {
             final UUID requestId = env.getMessageId();
             Duration timeout = env.getRpcTimeout();
 
-            synchronized(this) {
+            synchronized (this) {
                 log.debug(String.format("Adding requestId %s to the RPC Filter list", requestId.toString()));
                 sentRequests.add(requestId);
             }
 
-            if (timeout == Duration.ZERO)
-            {
+            if (timeout == Duration.ZERO) {
                 Timer gc = new Timer(true);
                 gc.schedule(new TimerTask() {
 
-					@Override
-					public void run() {
-						requestTimeout_GarbageCollect(requestId);
-					}
-                	
-                	}, 
-                	timeout.getMillis()*2);
-            }
-            else
-            {
+                    @Override
+                    public void run() {
+                        requestTimeout_GarbageCollect(requestId);
+                    }
+
+                }, timeout.getMillis() * 2);
+            } else {
                 log.warn(String.format(
-                    "Request %s was sent without a timeout: it will never be removed from the RPC Filter list", 
-                    requestId.toString()));
+                                "Request %s was sent without a timeout: it will never be removed from the RPC Filter list",
+                                requestId.toString()));
             }
         }
-	}
+    }
 
-	@Override
-	public boolean processInbound(ProcessingContext context) {
-        
-		boolean ourOwnRequest = false;
-		cmf.bus.berico.EnvelopeHelper env = new cmf.bus.berico.EnvelopeHelper(context.getEnvelope());
-        
-        try
-        {
-            if (env.IsRequest())
-            {
+    @Override
+    public boolean processInbound(ProcessingContext context) {
+
+        boolean ourOwnRequest = false;
+        cmf.bus.berico.EnvelopeHelper env = new cmf.bus.berico.EnvelopeHelper(context.getEnvelope());
+
+        try {
+            if (env.IsRequest()) {
                 UUID requestId = env.getMessageId();
 
-                synchronized(this) {
-                    if (sentRequests.contains(requestId))
-                    {
+                synchronized (this) {
+                    if (sentRequests.contains(requestId)) {
                         log.info("Filtering out our own request: " + requestId.toString());
                         ourOwnRequest = true;
                     }
                 }
             }
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             log.error("Failed to inspect an incoming event for potential filtering", ex);
         }
 
         return !ourOwnRequest;
-	}
-	
-    public void requestTimeout_GarbageCollect(UUID requestId)
-    {
+    }
+
+    public void requestTimeout_GarbageCollect(UUID requestId) {
         synchronized (this) {
             log.debug(String.format("Removing requestId %s from the RPC Filter list", requestId.toString()));
             sentRequests.remove(requestId);
