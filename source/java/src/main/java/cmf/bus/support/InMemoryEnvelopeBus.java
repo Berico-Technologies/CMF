df72d905317cfb8e1878b47f01b9545e0f944d8b
Richard Clayton <rclayton@bericotechnologies.com>
2012-10-24 14:27:14 -0400

    Added a check to see if the Filter Predicate on Registrations is null for the InMemoryEventBus

--- source/java/src/main/java/cmf/bus/support/InMemoryEnvelopeBus.java
+++ source/java/src/main/java/cmf/bus/support/InMemoryEnvelopeBus.java
@@ -15,7 +15,9 @@ public class InMemoryEnvelopeBus implements IEnvelopeBus {
         registrationList = new LinkedList<IRegistration>();
     }
 
-    protected void dispatch(final Envelope envelope, final List<IRegistration> registrationList) {
+    protected void dispatch(
+    		final Envelope envelope, 
+    		final List<IRegistration> registrationList) {
         new Thread() {
 
             @Override
@@ -44,9 +46,13 @@ public class InMemoryEnvelopeBus implements IEnvelopeBus {
     public synchronized void send(Envelope envelope) {
         List<IRegistration> registrations = new LinkedList<IRegistration>();
         for (IRegistration registration : registrationList) {
-            if (registration.getFilterPredicate().filter(envelope)) {
-                registrations.add(registration);
-            }
+        		
+        		if (null != registration.getFilterPredicate() 
+        			&& registration.getFilterPredicate().filter(envelope)){
+                   registrations.add(registration);
+        		} else {
+        			registrations.add(registration);
+        		}
         }
         dispatch(envelope, registrations);
     }
