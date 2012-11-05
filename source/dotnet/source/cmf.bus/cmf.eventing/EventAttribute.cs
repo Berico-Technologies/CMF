cefd1799140f27dee87ffbdaf674b108403e5f52
John Ruiz <jruiz@johnruiz.com>
2012-11-05 17:30:39 -0500

    Added the Event annotation (or Attribute) to control headers

---
+++ source/dotnet/source/cmf.bus/cmf.eventing/EventAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cmf.eventing
+{
+    public class EventAttribute : Attribute
+    {
+        public string EventType { get; protected set; }
+        public string EventTopic { get; protected set; }
+
+
+        public EventAttribute(string eventTopic = null, string eventType = null)
+        {
+            this.EventType = eventType;
+            this.EventTopic = eventTopic;
+        }
+    }
+}
