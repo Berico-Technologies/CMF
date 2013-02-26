fb8c7987df694b6eb0f0b413e0143361d06205d7
John Ruiz <jruiz@johnruiz.com>
2013-02-25 21:13:23 -0500

    Code compiles, but tests fail

--- java/src/test/java/cmf/bus/berico/DefaultEnvelopeBusTest.java
+++ java/src/test/java/cmf/bus/berico/DefaultEnvelopeBusTest.java
@@ -1,14 +1,16 @@
 package cmf.bus.berico;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InOrder;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import cmf.bus.Envelope;
 import cmf.bus.IRegistration;
@@ -23,124 +25,105 @@ public class DefaultEnvelopeBusTest {
 		new DefaultEnvelopeBus(transportProvider);
 		
 		verify(transportProvider).onEnvelopeReceived(
-			any(InboundEnvelopeProcessorCallback.class));
+			any(IEnvelopeReceivedCallback.class));
 	}
 
 	@Test
-	@SuppressWarnings("unchecked")
 	public void processor_order_is_preserved_by_processInbound() {
+		
+		// mock up some processors
+		IEnvelopeProcessor iep1 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor iep2 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor iep3 = mock(IEnvelopeProcessor.class);
+		
+		iep1.processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		iep2.processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		iep3.processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		
+		
+		
+		ITransportProvider transportProvider = mock(ITransportProvider.class);
+		
+		DefaultEnvelopeBus bus = new DefaultEnvelopeBus(transportProvider);
+			new DefaultEnvelopeBus(transportProvider, 
+				Arrays.asList(
+					new IEnvelopeProcessor[]{ iep1, iep2, iep3 }),
+				Arrays.asList(
+					new IEnvelopeProcessor[0]));
+		
+		InOrder inorder = inOrder(iep1, iep2, iep3);
+			
+		try {
+			bus.processEnvelope(
+					mock(EnvelopeContext.class), 
+					Arrays.asList(new IEnvelopeProcessor[]{ iep1, iep2, iep3 }),
+					mock(IContinuationCallback.class));
+		} catch (Exception e) {
+			Assert.fail("ProcessEnvelope threw an exception: " + e.toString());
+		}
+		
+		inorder.verify(iep1).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		inorder.verify(iep2).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		inorder.verify(iep3).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+	}
+
+	@Test
+	public void inbound_processor_stops_processing_envelope_if_a_processor_fails(){
 		
 		IEnvelopeProcessor iep1 = mock(IEnvelopeProcessor.class);
 		IEnvelopeProcessor iep2 = mock(IEnvelopeProcessor.class);
 		IEnvelopeProcessor iep3 = mock(IEnvelopeProcessor.class);
 		
+		List<IEnvelopeProcessor> processorChain = new LinkedList<IEnvelopeProcessor>();
+		processorChain.add(iep1);
+		processorChain.add(iep2);
+		processorChain.add(iep3);
 		
-		when(iep1.processInbound(any(Envelope.class), any(Map.class)))
-			.thenReturn(true);
+		// setup the first mock to call its continuation method
+		doAnswer(new Answer() {
+			public Object answer(InvocationOnMock invocation) throws Exception {
+				Object[] args = invocation.getArguments();
+				IContinuationCallback continuation = (IContinuationCallback)args[1];
+				continuation.continueProcessing();
+				return null;
+			}})
+			.when(iep1).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
 		
-		when(iep2.processInbound(any(Envelope.class), any(Map.class)))
-		.thenReturn(true);
+		// setup the second mock to NOT call its continuation method
+		doAnswer(new Answer() {
+			public Object answer(InvocationOnMock invocation) {
+				return null;
+			}})
+			.when(iep1).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
 		
-		when(iep3.processInbound(any(Envelope.class), any(Map.class)))
-		.thenReturn(true);
-		
-		InOrder inorder = inOrder(iep1, iep2, iep3);
 		
 		ITransportProvider transportProvider = mock(ITransportProvider.class);
 		
-		DefaultEnvelopeBus bus = 
-			new DefaultEnvelopeBus(transportProvider, 
-				Arrays.asList(
-					new IInboundEnvelopeProcessor[]{ iep1, iep2, iep3 }),
-				Arrays.asList(
-					new IOutboundEnvelopeProcessor[0]));
-		
-		Envelope envelope = mock(Envelope.class);
-		
-		boolean wasCompleted = bus.processInbound(envelope);
-		
-		assertTrue(wasCompleted);
-		
-		Map<String, Object> contextExample = new HashMap<String, Object>();
-		
-		inorder.verify(iep1).processInbound(envelope, contextExample);
-		inorder.verify(iep2).processInbound(envelope, contextExample);
-		inorder.verify(iep3).processInbound(envelope, contextExample);
-	}
+		DefaultEnvelopeBus bus = new DefaultEnvelopeBus(transportProvider);
 
-	@Test
-	@SuppressWarnings("unchecked")
-	public void inbound_processor_stops_processing_envelope_if_a_processor_fails(){
+		try {
+			bus.processEnvelope(
+					any(EnvelopeContext.class), 
+					processorChain,
+					new IContinuationCallback() {
+
+						@Override
+						public void continueProcessing() throws Exception {
+							Assert.fail("The EnvelopeBus's processEnvelope method should not have called the continuation callback");
+						}
+						
+					});
+		} catch (Exception e) {
+			Assert.fail("The EnvelopeBus threw an exception: " + e.toString());
+		}
 		
-		IInboundEnvelopeProcessor iep1 = mock(IInboundEnvelopeProcessor.class);
-		IInboundEnvelopeProcessor iep2 = mock(IInboundEnvelopeProcessor.class);
-		IInboundEnvelopeProcessor iep3 = mock(IInboundEnvelopeProcessor.class);
-		
-		when(iep1.processInbound(any(Envelope.class), any(Map.class)))
-			.thenReturn(true);
-		
-		// Fails to complete processing (returns false)
-		when(iep2.processInbound(any(Envelope.class), any(Map.class)))
-		.thenReturn(false);
-		
-		when(iep3.processInbound(any(Envelope.class), any(Map.class)))
-		.thenReturn(true);
-		
-		ITransportProvider transportProvider = mock(ITransportProvider.class);
-		
-		DefaultEnvelopeBus bus = 
-			new DefaultEnvelopeBus(transportProvider, 
-				Arrays.asList(
-					new IInboundEnvelopeProcessor[]{ iep1, iep2, iep3 }),
-				Arrays.asList(
-					new IOutboundEnvelopeProcessor[0]));
-		
-		Envelope envelope = mock(Envelope.class);
-		
-		boolean wasCompleted = bus.processInbound(envelope);
-		
-		assertFalse(wasCompleted);
-		
-		Map<String, Object> contextExample = new HashMap<String, Object>();
-		
-		verify(iep1).processInbound(envelope, contextExample);
-		
-		verify(iep2).processInbound(envelope, contextExample);
-		
+		verify(iep1).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
+		verify(iep2).processEnvelope(any(EnvelopeContext.class), any(IContinuationCallback.class));
 		verifyZeroInteractions(iep3);
 	}
 	
 	
-	@Test
-	public void processor_order_is_preserved_by_processOutbound() {
-		
-		IOutboundEnvelopeProcessor oep1 = mock(IOutboundEnvelopeProcessor.class);
-		IOutboundEnvelopeProcessor oep2 = mock(IOutboundEnvelopeProcessor.class);
-		IOutboundEnvelopeProcessor oep3 = mock(IOutboundEnvelopeProcessor.class);
-		
-		ITransportProvider transportProvider = mock(ITransportProvider.class);
-		
-		DefaultEnvelopeBus bus = 
-			new DefaultEnvelopeBus(transportProvider, 
-				Arrays.asList(
-					new IInboundEnvelopeProcessor[0]),
-				Arrays.asList(
-					new IOutboundEnvelopeProcessor[]{ oep1, oep2, oep3 }));
-		
-		InOrder inorder = inOrder(oep1, oep2, oep3);
-		
-		Envelope envelope = mock(Envelope.class);
-		
-		bus.processOutbound(envelope);
-		
-		Map<String, Object> contextExample = new HashMap<String, Object>();
-		
-		inorder.verify(oep1).processOutbound(envelope, contextExample);
-		inorder.verify(oep2).processOutbound(envelope, contextExample);
-		inorder.verify(oep3).processOutbound(envelope, contextExample);
-	}
-
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void register_throws_exception_if_registration_is_null() throws Exception {
 		
 		ITransportProvider transportProvider = mock(ITransportProvider.class);
@@ -175,7 +158,7 @@ public class DefaultEnvelopeBusTest {
 	}
 
 	@Test
-	public void send_passes_envelope_to_processOutbound_and_then_transport_provider() throws Exception{
+	public void send_passes_envelope_to_processEnvelope_and_then_transport_provider() throws Exception{
 		
 		ITransportProvider transportProvider = mock(ITransportProvider.class);
 		
@@ -187,8 +170,7 @@ public class DefaultEnvelopeBusTest {
 		
 		bus.send(envelope);
 		
-		inorder.verify(bus).processOutbound(envelope);
-		
+		inorder.verify(bus).send(envelope);
 		inorder.verify(transportProvider).send(envelope);
 	}
 
@@ -219,22 +201,22 @@ public class DefaultEnvelopeBusTest {
 	@Test
 	public void tranport_provider_and_processors_are_disposed_when_bus_dispose_method_is_called(){
 		
-		IInboundEnvelopeProcessor iep1 = mock(IInboundEnvelopeProcessor.class);
-		IInboundEnvelopeProcessor iep2 = mock(IInboundEnvelopeProcessor.class);
-		IInboundEnvelopeProcessor iep3 = mock(IInboundEnvelopeProcessor.class);
+		IEnvelopeProcessor iep1 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor iep2 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor iep3 = mock(IEnvelopeProcessor.class);
 		
-		IOutboundEnvelopeProcessor oep1 = mock(IOutboundEnvelopeProcessor.class);
-		IOutboundEnvelopeProcessor oep2 = mock(IOutboundEnvelopeProcessor.class);
-		IOutboundEnvelopeProcessor oep3 = mock(IOutboundEnvelopeProcessor.class);
+		IEnvelopeProcessor oep1 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor oep2 = mock(IEnvelopeProcessor.class);
+		IEnvelopeProcessor oep3 = mock(IEnvelopeProcessor.class);
 		
 		ITransportProvider transportProvider = mock(ITransportProvider.class);
 		
 		DefaultEnvelopeBus bus = 
 			new DefaultEnvelopeBus(transportProvider, 
 				Arrays.asList(
-					new IInboundEnvelopeProcessor[]{ iep1, iep2, iep3 }),
+					new IEnvelopeProcessor[]{ iep1, iep2, iep3 }),
 				Arrays.asList(
-					new IOutboundEnvelopeProcessor[]{ oep1, oep2, oep3 }));
+					new IEnvelopeProcessor[]{ oep1, oep2, oep3 }));
 		
 		InOrder inorder = inOrder(transportProvider, iep1, iep2, iep3, oep1, oep2, oep3);
 		
