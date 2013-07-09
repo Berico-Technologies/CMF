250e187e92c96a8f609036d097074ce27c860803
Jared Holmberg <jholmberg@bericotechnologies.com>
2013-07-08 21:55:09 -0400

    IsLast flag no longer needed as it will be replaced by an object to indicate to the event bus that the last event was received and the stream can be closed.

--- java/cmf-eventing-api/src/main/java/cmf/eventing/patterns/streaming/StreamingEnvelopeConstants.java
+++ java/cmf-eventing-api/src/main/java/cmf/eventing/patterns/streaming/StreamingEnvelopeConstants.java
@@ -4,5 +4,4 @@ package cmf.eventing.patterns.streaming;
 public class StreamingEnvelopeConstants {
     public static final String SEQUENCE_ID = "cmf.bus.message.pattern#streaming.sequenceId";
     public static final String POSITION = "cmf.bus.message.pattern#streaming.position";
-    public static final String IS_LAST = "cmf.bus.message.pattern#streaming.isLast";
 }
